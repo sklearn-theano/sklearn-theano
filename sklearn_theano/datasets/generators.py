385eb6888e890f633d0df516d254a1e76dd52010
Kyle Kastner <kastnerkyle@gmail.com>
2014-08-22 06:31:39 -0500

    Updated generators to include bias

--- sklearn_theano/datasets/generators.py
+++ sklearn_theano/datasets/generators.py
@@ -12,55 +12,43 @@ from sklearn.utils import check_random_state
 from ..base import (Feedforward, fuse)
 from ..datasets import get_dataset_dir, download
 
-GENERATOR_WEIGHTS_PATH = get_dataset_dir("adversarial_weights")
+GENERATORS_PATH = get_dataset_dir("adversarial_weights")
 
-MNIST_NETWORK_WEIGHT_FILE = 'mnist/generator_weights.pkl'
-MNIST_NETWORK_FILTER_SHAPES = np.array([(96, 3, 11, 11),
-                                        (256, 96, 5, 5),
-                                        (512, 256, 3, 3),
-                                        (1024, 512, 3, 3),
-                                        (1024, 1024, 3, 3),
-                                        (3072, 1024, 6, 6),
-                                        (4096, 3072, 1, 1),
-                                        (1000, 4096, 1, 1)])
-MNIST_NETWORK_BIAS_SHAPES = MNIST_NETWORK_FILTER_SHAPES[:, 0]
-MNIST_NETWORK = (MNIST_NETWORK_WEIGHT_FILE,
-                 MNIST_NETWORK_FILTER_SHAPES,
-                 MNIST_NETWORK_BIAS_SHAPES)
+MNIST_NETWORK_FILE = os.path.join('mnist', 'generator_params.jb')
 
 
 def _get_generator_weights():
     url = "https://dl.dropboxusercontent.com/u/15378192/adversarial_generator_weights.zip"
-    if not os.path.exists(GENERATOR_WEIGHTS_PATH):
-        os.makedirs(GENERATOR_WEIGHTS_PATH)
-    full_path = os.path.join(GENERATOR_WEIGHTS_PATH,
+    if not os.path.exists(GENERATORS_PATH):
+        os.makedirs(GENERATORS_PATH)
+    full_path = os.path.join(GENERATORS_PATH,
                              "adversarial_generator_weights.zip")
     if not os.path.exists(full_path):
         download(url, full_path, progress_update_percentage=1)
         zip_obj = zipfile.ZipFile(full_path, 'r')
-        zip_obj.extractall(GENERATOR_WEIGHTS_PATH)
+        zip_obj.extractall(GENERATORS_PATH)
         zip_obj.close()
 
 
-def fetch_mnist_weights(weights_file=None):
-    fname, weight_shapes, bias_shapes = MNIST_NETWORK
+def fetch_mnist_weights():
+    fname = MNIST_NETWORK_FILE
 
-    if weights_file is None:
-        weights_file = os.path.join(GENERATOR_WEIGHTS_PATH, fname)
-        if not os.path.exists(weights_file):
-            _get_generator_weights()
+    full_path = os.path.join(GENERATORS_PATH, fname)
+    if not os.path.exists(full_path):
+        _get_generator_weights()
 
-    weights = load(weights_file)
-    return weights
+    all_params = load(full_path)
+    weights = all_params[::2]
+    biases = all_params[1::2]
+    return weights, biases
 
 
-def _get_mnist_architecture(weights=None):
-    if weights is None:
-        weights = fetch_mnist_weights()
+def _get_mnist_architecture():
+    weights, biases = fetch_mnist_weights()
 
-    architecture = [Feedforward(weights[0], activation='relu'),
-                    Feedforward(weights[1], activation='relu'),
-                    Feedforward(weights[2], activation='sigmoid')]
+    architecture = [Feedforward(weights[0], biases[0], activation='relu'),
+                    Feedforward(weights[1], biases[1], activation='relu'),
+                    Feedforward(weights[2], biases[2], activation='sigmoid')]
     return architecture
 
 
