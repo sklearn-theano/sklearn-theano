55cc8ff796ad8a1aa5ffe6a1344ebca5a613c24d
Kyle Kastner <kastnerkyle@gmail.com>
2014-08-25 08:12:07 -0500

    Added cifar generator

--- sklearn_theano/datasets/__init__.py
+++ sklearn_theano/datasets/__init__.py
@@ -5,9 +5,11 @@ from .base import download
 from .base import load_sample_image
 from .base import load_sample_images
 from .generators import fetch_mnist_generated
+from .generators import fetch_cifar_fully_connected_generated
 
 __all__ = ['fetch_asirra',
            'fetch_mnist_generated',
+           'fetch_cifar_fully_connected_generated',
            'load_sample_images',
            'load_sample_image',
            'get_dataset_dir',
--- sklearn_theano/datasets/generators.py
+++ sklearn_theano/datasets/generators.py
@@ -16,6 +16,14 @@ GENERATORS_PATH = get_dataset_dir("adversarial_weights")
 
 MNIST_NETWORK_FILE = os.path.join('mnist', 'generator_params.jb')
 
+CIFAR_FULLY_CONNECTED_NETWORK_FILE = os.path.join('cifar10_fully_connected',
+                                                  'generator_params.jb')
+
+
+def _normalize(X):
+    return (X - X.min(axis=1)[:, np.newaxis]) / (X.max(axis=1) -
+                                                 X.min(axis=1))[:, np.newaxis]
+
 
 def _get_generator_weights():
     url = "https://dl.dropboxusercontent.com/u/15378192/adversarial_generator_weights.zip"
@@ -84,4 +92,47 @@ def fetch_mnist_generated(n_samples=1000, random_state=None):
     rng = check_random_state(random_state)
     generator_func = _get_mnist_fprop()
     X = 2 * np.sqrt(3) * rng.rand(n_samples, 100).astype('float32') - np.sqrt(3)
-    return generator_func(X)[0]
+    X_tf = generator_func(X)[0]
+    X_tf = _normalize(X_tf)
+    X_tf = X_tf.reshape(len(X_tf), 28, 28)
+    return X_tf
+
+
+def fetch_cifar_fully_connected_weights():
+    fname = CIFAR_FULLY_CONNECTED_NETWORK_FILE
+
+    full_path = os.path.join(GENERATORS_PATH, fname)
+    if not os.path.exists(full_path):
+        _get_generator_weights()
+
+    all_params = load(full_path)
+    weights = all_params[::2]
+    biases = all_params[1::2]
+    return weights, biases
+
+
+def _get_cifar_fully_connected_architecture():
+    weights, biases = fetch_cifar_fully_connected_weights()
+
+    architecture = [Feedforward(weights[0], biases[0], activation='relu'),
+                    Feedforward(weights[1], biases[1], activation='sigmoid'),
+                    Feedforward(weights[2], biases[2], activation='linear')]
+    return architecture
+
+
+def _get_cifar_fully_connected_fprop(output_layers=[-1]):
+    arch = _get_cifar_fully_connected_architecture()
+    expressions, input_var = fuse(arch, fuse_dim=2,
+                                  output_expressions=output_layers)
+    fprop = theano.function([input_var], expressions)
+    return fprop
+
+
+def fetch_cifar_fully_connected_generated(n_samples=1000, random_state=None):
+    rng = check_random_state(random_state)
+    generator_func = _get_cifar_fully_connected_fprop()
+    X = rng.randn(n_samples, 100).astype('float32')
+    X_tf = generator_func(X)[0]
+    X_tf = _normalize(X_tf)
+    X_tf = X_tf.reshape(len(X_tf), 3, 32, 32).transpose(0, 2, 3, 1)
+    return (255 * X_tf).astype('uint8')
