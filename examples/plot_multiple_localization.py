74cdf8efb6ca051ad45f757db799b59205516a9e
Kyle Kastner <kastnerkyle@gmail.com>
2014-09-01 16:35:24 -0500

    Fixed issue with localization example

--- examples/plot_multiple_localization.py
+++ examples/plot_multiple_localization.py
@@ -1,13 +1,18 @@
+import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
 from sklearn_theano.datasets import load_sample_image
 from sklearn_theano.feature_extraction import OverfeatLocalizer
+from sklearn.mixture import GMM
 
 
-def convert_points_to_box(points, color, alpha):
-    upper_left_point = (points[:, 0].min(), points[:, 1].min())
-    width = points[:, 0].max() - points[:, 0].min()
-    height = points[:, 1].max() - points[:, 1].min()
+def convert_gmm_to_box(gmm, color, alpha):
+    midpoint = gmm.means_
+    std = 3 * np.sqrt(clf.covars_)
+    width = std[:, 0]
+    height = std[:, 1]
+    upper_left_point = (midpoint[:, 0] - width // 2,
+                        midpoint[:, 1] - height // 2)
     return Rectangle(upper_left_point, width, height, ec=color,
                      fc=color, alpha=alpha)
 
@@ -19,15 +24,18 @@ clf = OverfeatLocalizer(top_n=1,
 points = clf.predict(X.astype('float32'))
 dog_points = points[0]
 cat_points = points[1]
-cat_box = convert_points_to_box(cat_points, 'steelblue', .4)
-dog_box = convert_points_to_box(dog_points, 'darkred', .4)
 
 plt.imshow(X)
 plt.title("Cat and dog localization")
 ax = plt.gca()
 ax.autoscale(enable=False)
-ax.add_patch(cat_box)
+clf = GMM()
+clf.fit(dog_points)
+dog_box = convert_gmm_to_box(clf, "darkred", .6)
+clf.fit(cat_points)
+cat_box = convert_gmm_to_box(clf, "steelblue", .6)
 ax.add_patch(dog_box)
+ax.add_patch(cat_box)
 ax.get_xaxis().set_ticks([])
 ax.get_yaxis().set_ticks([])
 plt.show()
