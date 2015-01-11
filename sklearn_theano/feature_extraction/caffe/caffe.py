46c367ca95b725267acc1de1c8968c122c10b871
Michael Eickenberg <michael.eickenberg@gmail.com>
2015-01-11 23:27:24 +0100

    WIP parse_caffe_model almost working

--- sklearn_theano/feature_extraction/caffe/caffe.py
+++ sklearn_theano/feature_extraction/caffe/caffe.py
@@ -1,6 +1,10 @@
 """Makes .caffemodel files readable for sklearn-theano"""
 import os
 import numpy as np
+from collections import OrderedDict
+import theano
+import theano.tensor as T
+
 
 def _compile_caffe_protobuf(caffe_proto=None,
                            proto_src_dir=None,
@@ -101,15 +105,105 @@ def _parse_caffe_model(caffe_model):
                             for blob in layer_blobs_raw]
     top_blobs = [l.top for l in layers_raw]
     bottom_blobs = [l.bottom for l in layers_raw]
+    pooling_info = [_read_pooling_param(l.pooling_param) for l in layers_raw]
 
-    return layers_raw, layer_names, layer_type_names, bottom_blobs, top_blobs, layer_blobs_ndarrays
+    output = (layer_names, layer_type_names, bottom_blobs,
+              top_blobs, layer_blobs_ndarrays, pooling_info)
 
-from sklearn_theano.base import (Convolution, Relu, MaxPool)
+    return output
 
 
+from sklearn_theano.base import (Convolution, Relu, MaxPool, FancyMaxPool)
+
+
+def parse_caffe_model(caffe_model, float_dtype='float32'):
+    """Reads a .caffemodel file and returns a list of sklearn-theano
+    operators.
+
+    Parameters
+    ==========
+
+    caffe_model: string or binary google protobuffer object
+        file or binary protobuf object specifying the caffe model.
+
+    Returns
+    =======
+
+    parsed_caffe_model: List of sklearn-theano operators ready to be fused.
+
+    Notes
+    =====
+
+    This parser understands the caffe layers
+    DATA
+    CONVOLUTION
+    RELU
+    POOLING
+
+    """
+
+    raw_parsed = _parse_caffe_model(caffe_model)
+
+    layers = OrderedDict()
+    # inputs = OrderedDict()
+    blobs = OrderedDict()
+
+    for i, (layer_name, layer_type, bottom_blobs,
+            top_blobs, layer_blobs, pooling_info
+            ) in enumerate(zip(*raw_parsed)):
+        if layer_type == 'DATA':
+            # DATA layers contain input data in top_blobs, create input
+            # variables, float for 'data' and int for 'label'
+            for data_blob_name in top_blobs:
+                if data_blob_name == 'label':
+                    blobs['label'] = T.ivector()
+                else:
+                    blobs[data_blob_name] = T.tensor4(dtype=float_dtype)
+        elif layer_type == 'CONVOLUTION':
+            # CONVOLUTION layers take input from bottom_blob, convolve with
+            # layer_blobs[0], and add bias layer_blobs[1]
+            conv_filter = layer_blobs[0].astype(float_dtype)
+            conv_bias = layer_blobs[1].astype(float_dtype).ravel()
+            convolution_input = blobs[bottom_blobs[0]]
+            import IPython
+            IPython.embed()
+            convolution = Convolution(conv_filter, biases=conv_bias,
+                                      activation=None, subsample=None,
+                                      input_dtype=float_dtype)
+            convolution._build_expression(convolution_input)
+            layers[layer_name] = convolution
+            blobs[top_blobs[0]] = convolution
+        elif layer_type == "RELU":
+            # RELU layers take input from bottom_blobs, set everything
+            # negative to zero and write the result to top_blobs
+            relu_input = bottom_blobs[0]
+            relu = Relu()
+            relu._build_expression(relu_input)
+            layers[layer_name] = relu
+            blobs[top_blobs[0]] = relu
+        elif layer_type == "POOLING":
+            # POOLING layers take input from bottom_blobs, perform max
+            # pooling according to stride and kernel size information
+            # and write the result to top_blobs
+            pooling_input = bottom_blobs[0]
+            kernel_size, stride = pooling_info
+            pooling = FancyMaxPool(kernel_size, stride)
+            pooling._build_expression(pooling_input)
+            layers[layer_name] = pooling
+            blobs[top_blobs[0]] = pooling
+        elif layer_type == "DROPOUT":
+            # DROPOUT may figure in some networks, but it is only relevant
+            # at the learning stage, not at the prediction stage.
+            pass
+        elif layer_type == "SOFTMAX_LOSS":
+            # SOFTMAX_LOSS is used at training time. At prediction time, we
+            # should replace it with a soft max.
+            pass
+
+    return layers, blobs
 
 if __name__ == "__main__":
-    pb = _parse_caffe_model("/home/me/Downloads/cifar10_nin.caffemodel")
+    pb = parse_caffe_model("/home/me232320/Downloads/cifar10_nin.caffemodel")
     import IPython
     IPython.embed()
 
